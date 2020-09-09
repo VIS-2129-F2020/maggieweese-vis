725b85916e248d984b832b5d9bde718fbb25ce39
unknown <maggie.weese@hsph.harvard.edu>
2020-09-09 09:10:32 -0400

    Uploading to check code pn tidycensus upload

---
+++ downloading tidycensus data.R
@@ -0,0 +1,12 @@
+remotes::install_github("walkerke/tidycensus")
+
+remotes::install_github("walkerke/tidycensus")
+
+
+
+
+install_github("walkerke/tidycensus")
+
+remotes::install_github("walkerke/tidycensus")
+
+install.packages(“remotes”)
